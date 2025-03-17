3eaf708898161446f347c1e3b5f2a5931a0d7146
ap058175 <ap058175@gmail.com>
2025-03-17 14:56:10 +0530

    mod() added

--- src/main/java/gfgamit32/Calculator.java
+++ src/main/java/gfgamit32/Calculator.java
@@ -18,6 +18,10 @@ public class Calculator {
 	public int div(int a, int b) {
 		return (a/b);
 	}
+	
+	public int mod(int a, int b) {
+		return (a%b);
+	}
 
 	public static void main(String[] args) {
 		
@@ -26,6 +30,7 @@ public class Calculator {
     System.out.println("Diff is :"+myCalc.diff(20,10));
     System.out.println("mul is :"+myCalc.mul(20,10));
     System.out.println("Div is :"+myCalc.div(20,10));
+    System.out.println("Mod is :"+myCalc.mod(20,10));
 	}
 
 }
