e42e081c408998d0759463d7da601ba848d4af36
ap058175 <ap058175@gmail.com>
2025-03-22 17:20:31 +0530

    new commit

--- src/test/java/gfgamit32/gfgamit.java
+++ src/test/java/gfgamit32/gfgamit.java
@@ -11,14 +11,14 @@ class gfgamit {
 	@Test
 	public void test_sum() {
 		assertEquals(30,calc.sum(20, 10));
-		assertEquals(2,calc.sum(1, 10));
+		assertEquals(11,calc.sum(1, 10));
 		assertEquals(0,calc.sum(-1, 1));
 		
 	}
 	public void test_diff() {
 		assertEquals(10,calc.diff(20, 10));
-		assertEquals(5,calc.sum(10, 5));
-		assertEquals(-2,calc.sum(-1, 1));
+		assertEquals(5,calc.diff(10, 5));
+		assertEquals(-2,calc.diff(-1, 1));
 		
 	}
 
